dd808d5bca066f072c4378f6f778f2cf386420db
Tony Butler <anthonywbutler1@gmail.com>
2022-07-07 15:15:02 +0200

    feat: add parent container to app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import ParentContainer from "./containers/ParentContainer";
 
 function App() {
   return (
     <div className="App">
-
+      <ParentContainer />
     </div>
   );
 }
